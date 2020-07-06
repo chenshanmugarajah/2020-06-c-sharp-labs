8832b39857556da8dfc0764b287548e33b6c567f
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-06 11:07:00 +0100

    Initial commit

---
+++ labs/lab_02_framework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_02_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chen");
+        }
+    }
+}
