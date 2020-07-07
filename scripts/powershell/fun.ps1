8976935d09277470b273cd4391e148d0597d5d2d
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-07 08:54:10 +0100

    menu.txt file added

--- scripts/powershell/fun.ps1
+++ scripts/powershell/fun.ps1
@@ -1,1 +1,20 @@
-echo "This is the fun script!"
+function global:newMenu {
+    mkdir menu-items
+    cd menu-items
+}
+
+function global:createItem {
+    $menuItem = $args[0]
+    $price = $args[1]
+
+    echo $price >> $menuItem 
+}
+
+createItem $args[0] $args[1]
+
+function global:collateMenu {
+    $menuitems = Get-Content C:\Users\
+    foreach ($item in $menuitems) {
+        echo $item >> fullmenu.txt
+    }
+}
