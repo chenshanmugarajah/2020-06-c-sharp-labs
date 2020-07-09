b7e9a78069dd3af05403ab6989953c35ae0c5668
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-09 19:02:10 +0100

    add tests and functionality for calculator class

---
+++ Calculator/CalculatorLib/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace CalculatorLib
+{
+    public class StaticCalculator
+    {
+        public static double add (double i, double k)
+        {
+            return i + k;
+        }
+
+        public static double subtract (double i, double k)
+        {
+            return i - k;
+        }
+
+        public static double multiply (double i, double k)
+        {
+            return i * k;
+        }
+
+        public static double divide (double i, double k)
+        {
+            return i / k;
+        }
+
+        public static double modulus (double i, double k)
+        {
+            return i % k;
+        }
+    }
+}
---
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using CalculatorLib;
+
+namespace CalculatorTests
+{
+    public class StaticCalculatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(10, 5, 15)]
+        [TestCase(13, 7, 20)]
+        [TestCase(-1, 5, 4)]
+        public void AddIsCorrect(int a, int b, int expected)
+        {
+            var actual = StaticCalculator.add(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, 2, 8)]
+        [TestCase(6, 11, -5)]
+        [TestCase(-1, 5, -6)]
+        public void SubtractIsCorrect(int a, int b, int expected)
+        {
+            var actual = StaticCalculator.subtract(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 5, 0)]
+        [TestCase(13, 5, 65)]
+        [TestCase(-1, 5, -5)]
+        public void MultiplyIsCorrect(int a, int b, int expected)
+        {
+            var actual = StaticCalculator.multiply(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, 5, 2)]
+        [TestCase(6, 2, 3)]
+        [TestCase(18, 6, 3)]
+        public void DivideIsCorrect(int a, int b, int expected)
+        {
+            var actual = StaticCalculator.divide(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, 4, 2)]
+        [TestCase(2, 3, 2)]
+        [TestCase(7, 3, 1)]
+        public void ModulusIsCorrect(int a, int b, int expected)
+        {
+            var actual = StaticCalculator.modulus(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
