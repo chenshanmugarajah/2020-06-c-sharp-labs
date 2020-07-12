b8ee8ecef76fa89c10e0695df39b36d324acfe0f
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-12 10:56:27 +0100

    fixed problem with homework and completed the task

---
+++ IterationExercises 2/IterationExercises/IterationExercises/Exercises.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace IterationLib
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ignore
+        }
+    }
+
+    public class Exercises
+    {
+        // returns the lowest number in the array nums
+        public static int Lowest(int[] nums)
+        {
+            if (nums.Length == 0) return int.MaxValue;
+            int ans = nums[0];
+            for (int i=0; i<nums.Length; i++)
+            {
+                if (nums[i] <= ans)
+                {
+                    ans = nums[i];
+                }
+            }
+            return ans;
+        }
+
+        // returns the sum of all numbers between 1 and n inclusive that are divisible by either 2 or 5
+        public static int SumEvenFive(int max)
+        {
+            int ans = 0;
+            for (int i=0; i<=max; i++)
+            {
+                if (i%2==0 || i%5==0)
+                {
+                    ans += i;
+                }
+            }
+            return ans;
+        }
+
+        // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
+        // all other letters are ignored
+        public static string CountLetters(string input)
+        {
+            int aCount = 0;
+            int bCount = 0;
+            int cCount = 0;
+            int dCount = 0;
+
+            foreach(char c in input.ToLower())
+            {
+                switch (c)
+                {
+                    case 'a': aCount++; break;
+                    case 'b': bCount++; break;
+                    case 'c': cCount++; break;
+                    case 'd': dCount++; break;
+                    default: break;
+                }
+            }
+
+            return $"A:{aCount} B:{bCount} C:{cCount} D:{dCount}";
+        }
+    }
+}
