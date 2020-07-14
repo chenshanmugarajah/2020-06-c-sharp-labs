79e2cda1cb2e5e844841b8f52b6b374a02234aef
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-14 08:45:06 +0100

    committed

---
+++ homework/C# core/CollectionsExercises/CollectionsExercises/CollectionsExercises.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsExercisesLib
+{
+    public class CollectionsExercises
+    {
+        /* removes and returns the next num entries in the queue, as a comma separated string */
+        public static string NextInQueue(int num, Queue<string> queue)
+        {
+            string peopleInQ = "";
+
+            for (int i=0; i<num; i++)
+            {
+                if (queue.Count == 0) break; 
+                if (i != 0) peopleInQ += ", ";
+                peopleInQ += queue.Peek();
+                queue.Dequeue();
+            }
+
+            return peopleInQ;
+        }
+
+        /* uses a Stack to create and return array of ints in reverse order to the one supplied */
+        public static int[] Reverse(int[] original)
+        {
+            var reverseStack = new Stack<int>();
+
+            for (int i=0; i<original.Length; i++)
+            {
+                reverseStack.Push(original[i]);
+            }
+
+            return reverseStack.ToArray();
+        }
+        // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
+        public static string CountDigits(string input)
+        {
+            var occurances = new Dictionary<string, int>();
+            string[] inputArray = input.Split("");
+
+            foreach(char s in input)
+            {
+                try
+                {
+                    int x = Int32.Parse(s.ToString());
+                    if (occurances.ContainsKey(s.ToString()))
+                    {
+                        occurances[s.ToString()] += 1;
+                    }
+                    else
+                    {
+                        occurances.Add(s.ToString(), 1);
+                    }
+                } catch (Exception e)
+                {
+                }
+            }
+
+            string output = "";
+
+            foreach(var item in occurances)
+            {
+                output += $"[{item.Key}, {item.Value}]";
+            }
+
+            return output;
+        }
+    }
+}
--- homework/C# core/DataTypesExercise/DataTypesExercise/DataTypesExercise/DataTypesPt2Lib.cs
+++ homework/C# core/DataTypesExercise/DataTypesExercise/DataTypesExercise/DataTypesPt2Lib.cs
@@ -10,21 +10,37 @@ namespace DataTypesPt2Lib
     {
         public static int AgeAt(DateTime birthDate, DateTime date)
         {
-            return -1;
+            if (date < birthDate)
+            {
+                throw new ArgumentException("Error - birthDate is in the future");
+            }
+
+            var age = date - birthDate;
+            int ageAt = (int)(age.Days / 365.25);
+           
+            return ageAt;   
         }
 
         public static string FormatDate(DateTime date)
         {
-            return string.Empty;
+            return date.ToString("yy/dd/MMM");
         }
         public static string GetMonthString(DateTime date)
         {
-            return string.Empty;
+            return date.ToString("MMMM");
         }
 
         public static string Fortune(Suit suit)
         {
-            return string.Empty;
+
+            switch (suit)
+            {
+                case Suit.CLUBS: return "And the seventh rule is if this is your first night at fight club, you have to fight.";
+                case Suit.DIAMONDS: return "Diamonds are a girls best friend";
+                case Suit.HEARTS: return "You've broken my heart";
+                case Suit.SPADES: return "Bucket and spade";
+                default: return "Test";
+            }
         }
     }
 }
