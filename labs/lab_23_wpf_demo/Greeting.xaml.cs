91e094864329ae7b8999020a512a7f7c73d86b46
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-07-17 16:26:37 +0100

    days work

---
+++ labs/lab_23_wpf_demo/Greeting.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_23_wpf_demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ButtonGood.IsChecked == true)
+            {
+                MessageBox.Show("They are fantastic!");
+            }
+            else if (ButtonBad.IsChecked == true)
+            {
+                MessageBox.Show("I hate them all!");
+            }
+        }
+    }
+}
