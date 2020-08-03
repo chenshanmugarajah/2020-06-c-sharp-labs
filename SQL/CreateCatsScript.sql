33810388368f3d06a68cb55726f4c0c298a82d50
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-03 12:29:48 +0100

    Added script to create cats table

---
+++ SQL/CreateCatsScript.sql
@@ -0,0 +1,22 @@
+﻿use master
+
+drop database if exists CatDB
+
+create database CatDB
+go
+
+use CatDB
+go
+
+create table Cats (
+	CatId int not null identity(1,1) primary key,
+	CatName nvarchar(50),
+	CatAge int,
+	CatDescription nvarchar(50)
+)
+
+insert into Cats values ('Ozzies', 15, 'Mainecoon')
+insert into Cats values ('Lucy', 3, 'Sphinx')
+insert into Cats values ('Jasper', 1, 'Great Cat')
+
+select * from Cats
---
+++ labs/lab_25_b_api_core_demo/Models/Categories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_25_b_api_core_demo.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
