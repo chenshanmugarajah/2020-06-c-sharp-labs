ba34734dfdb8a5245920ddcc0f8a2b49a84af658
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-12 12:25:02 +0100

    fetch functionality added

--- HTML/lab_04/assets/js/main.js
+++ HTML/lab_04/assets/js/main.js
@@ -1,1 +1,22 @@
 console.log("Hello from Main.js")
+
+var nasadata;
+
+fetch("https://images-api.nasa.gov/search?q=rover")
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        nasadata = data.collection.items;
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+document.getElementById()
