92facfbc416261ece7250463ac16c595d5f92ad9
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-12 13:02:00 +0100

    get request to api added and data displaying on html

--- HTML/lab_04/assets/js/main.js
+++ HTML/lab_04/assets/js/main.js
@@ -1,6 +1,3 @@
-console.log("Hello from Main.js")
-
-var nasadata;
 
 fetch("https://images-api.nasa.gov/search?q=rover")
   .then(
@@ -11,7 +8,8 @@ fetch("https://images-api.nasa.gov/search?q=rover")
         return;
       }
       response.json().then(function(data) {
-        nasadata = data.collection.items;
+        console.log(data.collection.items)
+        displayInfo(data.collection.items)
       });
     }
   )
@@ -19,4 +17,29 @@ fetch("https://images-api.nasa.gov/search?q=rover")
     console.log('Fetch Error :-S', err);
   });
 
-document.getElementById()
+  function displayInfo(nasadata) {
+
+    var nasainfo = document.getElementById("nasa-info");
+
+    for (var i=0; i<nasadata.length / 4; i++) {
+
+        var title = nasadata[i].data[0].title
+        var description = nasadata[i].data[0].description
+        var imagesrc = nasadata[i].links[0].href
+
+        
+        var heading = document.createElement("h3")
+        heading.innerText = title
+        var info = document.createElement("p")
+        info.innerText = description
+        var image = document.createElement("img")
+        image.src = imagesrc
+        
+        var infoDiv = document.createElement("div");
+        infoDiv.appendChild(heading)
+        infoDiv.appendChild(info)
+        infoDiv.appendChild(image)
+
+        nasainfo.appendChild(infoDiv)
+    }
+  }
